dc366f7e2a63c4d9362d64414d7b6cebf9205389
Jerikhorr <133670400+Jerikhorr@users.noreply.github.com>
2024-10-21 10:48:11 +0700

    folder path

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './**/*.php',
+    './assets/**/*.js',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
